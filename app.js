c56ae8c6d632ec04d922da3c72727313dc2c4a15
innoibad <muhammedibad125@gmail.com>
2026-09-15 14:03:40 -0600

    new

--- app.js
+++ app.js
@@ -34,4 +34,8 @@ app.get('/', (req, res) => {
     res.send('Backend is running on Vercel 🚀');
 });
 
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
 module.exports = app;
